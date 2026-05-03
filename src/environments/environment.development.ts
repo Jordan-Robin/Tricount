59d48e92a361b3f939292c5bfb0e8d1ab3bdac6d
Jordan Robin <126224637+Jordan-Robin@users.noreply.github.com>
2026-05-03 16:49:32 +0200

    Update Supabase configuration for development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: 'https://rzyfbqgrsrhfysqtisfo.supabase.co',
-  supabaseKey: 'sb_publishable_FVqwrLG3GY7EPTQI9MwBaQ_gQWXgrHY',
+  supabaseUrl: 'https://<project>.supabase.co',
+  supabaseKey: '<anon-key>',
 };
